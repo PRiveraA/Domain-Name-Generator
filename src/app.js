b6ea72969d3f0ff1a52bb9b91e2120d808069eb5
PRiveraA <priveraa94@gmail.com>
2023-02-15 10:36:49 +0000

    Initial commit?

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+let pronoun = ["the", "our"];
+let adjective = ["great", "big"];
+let noun = ["jogger", "raccoon"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let a = 0; a < adjective.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      let domain = `${pronoun[i]}${adjective[a]}${noun[n]}.com`;
+      console.log(domain);
+    }
+  }
+}
