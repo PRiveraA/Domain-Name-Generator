1edbc6ad015da2b61ebb222586e9ea42a534dac8
PRiveraA <priveraa94@gmail.com>
2023-02-15 11:20:03 +0000

    whatever

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
-// Results are supposed to show in the console
-
 let pronoun = ["the", "our"];
 let adjective = ["great", "big"];
 let noun = ["jogger", "raccoon"];
+let domains = [];
 
-for (let i = 0; i < pronoun.length; i++) {
+for (let p = 0; p < pronoun.length; p++) {
   for (let a = 0; a < adjective.length; a++) {
     for (let n = 0; n < noun.length; n++) {
-      let domain = `${pronoun[i]}${adjective[a]}${noun[n]}.com`;
-      console.log(domain);
+      let domain = `${pronoun[p]}${adjective[a]}${noun[n]}.com`;
+      domains.push(domain);
     }
   }
 }
+
+for (let p = domains.length - 1; p > 0; p--) {
+  const a = Math.floor(Math.random() * (p + 1));
+  [domains[p], domains[a]] = [domains[a], domains[p]];
+}
+
+domains.forEach(domain => console.log(domain));
